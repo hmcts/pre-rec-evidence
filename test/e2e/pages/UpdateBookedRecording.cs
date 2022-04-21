5fe0870aba3816d7df07c7e71dee7443f20516d3
AyishaPatel <98095477+AyishaPatel@users.noreply.github.com>
2022-04-21 09:38:33 +0100

    added tests for update case

---
+++ test/e2e/pages/UpdateBookedRecording.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace pre.test.pages
+{
+  public class UpdateBookedRecording : BasePage
+  {
+    
+    public UpdateBookedRecording(IPage page) : base(page)
+    {
+    }
+    private String stringCourt = "";
+    private String stringCase = "";
+   public async Task NavigateToBooking()
+    {
+
+      System.Console.WriteLine("this is"+ UpdateBookedRecordings._pagesetters.Page);
+      //var book = UpdateBookedRecordings._pagesetters.Page.Frame("fullscreen-app-host")
+       // .Locator("button:has-text(\"Book a Recording\")");
+
+      // await Task.Run(() => book.IsVisibleAsync().Result);
+       await Page.Frame("fullscreen-app-host").ClickAsync("button:has-text(\"Manage Recordings\")");
+    }
+
+    public async Task FindCaseToUpdate() {
+      var caseLocation = UpdateBookedRecordings._pagesetters.Page.Frame("fullscreen-app-host").Locator( "div:nth-child(1) > div.canvasContentDiv.container_1vt1y2p > div > div:nth-child(2)");
+      stringCase = caseLocation.TextContentAsync().Result.ToString().Trim();
+      stringCase = stringCase.Substring(stringCase.LastIndexOf(':') + 1);
+
+      await Page.Frame("fullscreen-app-host").ClickAsync("[placeholder='Search case ref']");
+      await Page.Frame("fullscreen-app-host").FillAsync("[placeholder='Search case ref']", $"{stringCase.Trim()}");
+    }
+    public async Task SearchCase()
+    {
+      var caseLocation = UpdateBookedRecordings._pagesetters.Page.Frame("fullscreen-app-host").Locator(
+        "div:nth-child(1) > div.canvasContentDiv.container_1vt1y2p > div > div:nth-child(2)");
+      stringCase = caseLocation.TextContentAsync().Result.ToString().Trim();
+      stringCase = stringCase.Substring(stringCase.LastIndexOf(':') + 1);
+      
+
+
+      var courtLocation = UpdateBookedRecordings._pagesetters.Page.Frame("fullscreen-app-host").Locator(
+        "div:nth-child(1) > div.canvasContentDiv.container_1vt1y2p > div > div:nth-child(4)");
+      stringCourt = courtLocation.TextContentAsync().Result.ToString().Trim();
+      stringCourt = stringCourt.Substring(stringCourt.LastIndexOf(':') + 1);
+
+
+
+      await Page.Frame("fullscreen-app-host").ClickAsync("button:has-text(\"Home\")");
+      await Page.Frame("fullscreen-app-host").ClickAsync("button:has-text(\"Book a Recording\")");
+      await Page.Frame("fullscreen-app-host").ClickAsync("[aria-label=\"Select\\ Court\"]");
+      System.Console.WriteLine("This is "+stringCourt);
+      await Page.Frame("fullscreen-app-host")
+        .ClickAsync($"[aria-label=\"Select\\ Court\\ items\"] div:has-text(\"{stringCourt}\")");
+
+      var caseInput = UpdateBookedRecordings._pagesetters.Page.Frame("fullscreen-app-host")
+        .Locator("[placeholder=\"Case\\ Number\\ \\\\\\ URN\"]");
+      await Task.Run(() => Assert.IsTrue(caseInput.IsVisibleAsync().Result));
+      await Page.IsVisibleAsync("[placeholder=\"Case\\ Number\\ \\\\\\ URN\"]");
+
+      await Page.Frame("fullscreen-app-host").ClickAsync("[placeholder=\"Case\\ Number\\ \\\\\\ URN\"]");
+      await Page.Frame("fullscreen-app-host")
+        .FillAsync("[placeholder=\"Case\\ Number\\ \\\\\\ URN\"]", $"{stringCase.Trim()}");
+    }
+    public async Task FindCase()
+    {
+
+
+      var caseLocation = UpdateBookedRecordings._pagesetters.Page.Frame("fullscreen-app-host")
+        .Locator("div.canvasContentDiv.container_1vt1y2p > div > div:nth-child(3)");
+      await Task.Run(() => Assert.That(caseLocation.TextContentAsync().Result, Does.Contain($"{stringCourt.Trim()}")));
+
+      var caseName = UpdateBookedRecordings._pagesetters.Page.Frame("fullscreen-app-host").Locator(
+        "#publishedCanvas div.canvasContentDiv.container_1vt1y2p div div:nth-child(1) div div div div div");
+      await Task.Run(() => Assert.That(caseName.TextContentAsync().Result, Does.Contain($"{stringCase.Trim()}")));
+    }
+    public async Task UpdateCase()
+    {
+        await Page.Frame("fullscreen-app-host").ClickAsync(".container_1f0sgyp div:nth-child(2) .react-knockout-control .appmagic-svg");
+        await Page.Frame("fullscreen-app-host").ClickAsync("button:has-text(\"Modify\")");
+
+
+      
+      // await Page.Frame("fullscreen-app-host").ClickAsync("[aria-label=\"Enter\\ your\\ Defendants\\,\\ comma\\ seperated\"]");
+      // await Page.Frame("fullscreen-app-host").FillAsync("[aria-label=\"Enter\\ your\\ Defendants\\,\\ comma\\ seperated\"]", "Updatedef 1,\nUpdatedef 2");
+      await Page.Frame("fullscreen-app-host").ClickAsync("[aria-label=\"Enter\\ your\\ Witnesses\\,\\ comma\\ seperated\"]");
+      await Page.Frame("fullscreen-app-host").FillAsync("[aria-label=\"Enter\\ your\\ Witnesses\\,\\ comma\\ seperated\"]","UpdateWitness 1,\nUpdateWitness 2");
+      var enabled = Page.Frame("fullscreen-app-host").Locator(":nth-match(button:has-text(\"Save\"), 2)");
+      //await Task.Run(() => Assert.IsTrue(enabled.IsEnabledAsync().Result));
+     // await Page.Frame("fullscreen-app-host").IsEnabledAsync(":nth-match(button:has-text(\"Save\"), 2)");
+      //await Page.Frame("fullscreen-app-host").ClickAsync("#publishedCanvas");
+      await Task.Run(() => Assert.That(enabled.TextContentAsync().Result, Does.Contain("Save")));
+      await UpdateBookedRecordings._pagesetters.Page.WaitForFunctionAsync(":nth-match(button:has-text(\"Save\"), 2)");
+      
+      //await Task.Run(() => Page.Frame("fullscreen-app-host").FocusAsync(":nth-match(button:has-text(\"Save\"), 2)"));
+      //await Page.Frame("fullscreen-app-host").ClickAsync(":nth-match(button:has-text(\"Save\"), 2)");
+      //await Page.Frame("fullscreen-app-host").ClickAsync(":nth-match(button:has-text(\"Save\"), 2)");;;;;
+    }
+
+
+
+
+
+
+
+
+
+   }
+}
---
+++ test/e2e/steps/UpdateBookedRecordings.cs
@@ -0,0 +1,72 @@
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using pre.test.pages;
+using pre.test.Hooks;
+
+
+namespace pre.test
+    {
+    [Binding]
+    public class UpdateBookedRecordings
+    {
+
+        public static BookRecording _bookrecording;
+        public static UpdateBookedRecording _updatebookedrecording;
+        public static PageSetters _pagesetters;
+        public UpdateBookedRecordings(PageSetters pageSetters)
+        {
+            _pagesetters = pageSetters;
+            _updatebookedrecording = new UpdateBookedRecording(_pagesetters.Page);
+        }
+
+
+
+
+
+[Given(@"I am on the book recordings page and I want to find an existing Case")]
+public async Task GivenIamonthebookrecordingspage()
+{
+	await _pagesetters.Page.GotoAsync("https://apps.powerapps.com/play/ee7bf58e-99c9-4a34-b57d-7137307231af?tenantId=531ff96d-0ae9-462a-8d2d-bec7c0b42082");
+    await _bookrecording.NavigateToBooking();
+}
+
+
+[When(@"I want to find an existing Cases and enter the case reference")]
+public async Task WhenIwanttofindanexistingCase()
+{
+	await _updatebookedrecording.SearchCase();
+}
+
+[Then(@"the application will check if the case reference already exists")]
+public async Task Thentheapplicationwillcheckif()
+{
+	await _updatebookedrecording.FindCase();
+    //await _updatebookedrecording.UpdateCase();
+}
+
+
+[Given(@"I have found an existing case")]
+public async Task GivenIhavefoundanexistingcase()
+{
+	await _bookrecording.NavigateToBooking();
+    await _updatebookedrecording.SearchCase();
+    await _updatebookedrecording.FindCase();
+}
+
+
+[When(@"I enter and save additional witnesses")]
+public async Task WhenIenterandsaveadditionalwitnesses()
+{
+	await _updatebookedrecording.UpdateCase();
+}
+
+
+[Then(@"the case will be updated with additional witnesses")]
+public async Task Thenthecasewillbeupdatedwithadditionalwitnesses()
+{
+	
+}
+
+        
+    }
+}
