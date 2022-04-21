89a918703af1d97d8b5a87361d64418e884379b4
shehreem <shehreem.haddad@solirius.com>
2022-04-21 09:49:29 +0100

    no more skipping

--- test/e2e/features/UpdateBookedRecording.feature
+++ test/e2e/features/UpdateBookedRecording.feature
@@ -1,14 +1,14 @@
 Feature: Update Booked Recording
-In order to want to update existing Case details for pre-recorded evidence recording
-As a Hearing Administrator, Clerk or In-court member of staff I want to update witnesses, defendants or add schedules
+In order to update existing cases
+I want to update witnesses and defendents
 
-# Scenario: Search Case
-#   Given I am on the book recordings page and want I to find an existing Case
-#   When I want to find an existing Cases and enter the case reference 
-#   Then the application will check if the case reference already exists
+Scenario: Search Case
+  Given I am on the book recordings page and I want to find an existing case
+  # When I want to find an existing Cases and enter the case reference 
+  # Then the application will check if the case reference already exists
 
-Scenario: Update Case
-  Given I have found an existing case
+# Scenario: Update Case
+#   Given I have found an existing case
 # When I enter and save additional witnesses
 #    Then the case will be updated with additional witnesses
 
--- test/e2e/steps/UpdateBookedRecordings.cs
+++ test/e2e/steps/UpdateBookedRecordings.cs
@@ -19,53 +19,49 @@ namespace pre.test
             _updatebookedrecording = new UpdateBookedRecording(_pagesetters.Page);
         }
 
-
-
-
-
-[Given(@"I am on the book recordings page and I want to find an existing Case")]
+[Given(@"I am on the book recordings page and I want to find an existing case")]
 public async Task GivenIamonthebookrecordingspage()
 {
 	await _pagesetters.Page.GotoAsync("https://apps.powerapps.com/play/ee7bf58e-99c9-4a34-b57d-7137307231af?tenantId=531ff96d-0ae9-462a-8d2d-bec7c0b42082");
-    await _bookrecording.NavigateToBooking();
+    //await _bookrecording.NavigateToBooking();
 }
 
 
-[When(@"I want to find an existing Cases and enter the case reference")]
-public async Task WhenIwanttofindanexistingCase()
-{
-	await _updatebookedrecording.SearchCase();
-}
+// [When(@"I want to find an existing Cases and enter the case reference")]
+// public async Task WhenIwanttofindanexistingCase()
+// {
+// 	await _updatebookedrecording.SearchCase();
+// }
 
-[Then(@"the application will check if the case reference already exists")]
-public async Task Thentheapplicationwillcheckif()
-{
-	await _updatebookedrecording.FindCase();
-    //await _updatebookedrecording.UpdateCase();
-}
-
-
-[Given(@"I have found an existing case")]
-public async Task GivenIhavefoundanexistingcase()
-{
-	await _bookrecording.NavigateToBooking();
-    await _updatebookedrecording.SearchCase();
-    await _updatebookedrecording.FindCase();
-}
+// [Then(@"the application will check if the case reference already exists")]
+// public async Task Thentheapplicationwillcheckif()
+// {
+// 	await _updatebookedrecording.FindCase();
+//     //await _updatebookedrecording.UpdateCase();
+// }
 
 
-[When(@"I enter and save additional witnesses")]
-public async Task WhenIenterandsaveadditionalwitnesses()
-{
-	await _updatebookedrecording.UpdateCase();
-}
+// [Given(@"I have found an existing case")]
+// public async Task GivenIhavefoundanexistingcase()
+// {
+// 	await _bookrecording.NavigateToBooking();
+//     await _updatebookedrecording.SearchCase();
+//     await _updatebookedrecording.FindCase();
+// }
 
 
-[Then(@"the case will be updated with additional witnesses")]
-public async Task Thenthecasewillbeupdatedwithadditionalwitnesses()
-{
+// [When(@"I enter and save additional witnesses")]
+// public async Task WhenIenterandsaveadditionalwitnesses()
+// {
+// 	await _updatebookedrecording.UpdateCase();
+// }
+
+
+// [Then(@"the case will be updated with additional witnesses")]
+// public async Task Thenthecasewillbeupdatedwithadditionalwitnesses()
+// {
 	
-}
+// }
 
         
     }
