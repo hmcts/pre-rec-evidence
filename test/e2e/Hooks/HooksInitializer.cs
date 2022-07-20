ea2f325011c95426de69437d74a355fa9b9996f4
shehreem <shehreem.haddad@solirius.com>
2022-07-20 14:36:45 +0100

    try

--- test/e2e/Hooks/HooksInitializer.cs
+++ test/e2e/Hooks/HooksInitializer.cs
@@ -102,7 +102,7 @@ namespace pre.test.Hooks
       browserType = "Chromium";
 
       playwright = await Playwright.CreateAsync();
-      BrowserTypeLaunchOptions typeLaunchOptions = new BrowserTypeLaunchOptions { Headless = headless, SlowMo = 50};
+      BrowserTypeLaunchOptions typeLaunchOptions = new BrowserTypeLaunchOptions { Headless = headless, SlowMo = 350};
       browser = await playwright.Chromium.LaunchAsync(typeLaunchOptions);
       context = await browser.NewContextAsync(new BrowserNewContextOptions { StorageStatePath = $"{authPath}", RecordVideoDir = $"{videoPath}"});
       _context.Page = await context.NewPageAsync();
